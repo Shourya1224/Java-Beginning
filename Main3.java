871c19c53b435e0deabc94e76e3ee5f1e5d42186
Shourya1224 <shourya21m@gmail.com>
2025-06-23 22:02:19 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("The values provided are");
+        Scanner sc = new Scanner (System.in);
+       
+        System.out.println("Enter the first digit");
+         int a = sc.nextInt();
+     
+        System.out.println(" Enter the second digit");
+           int b =sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+        
+    
+      
+
+    }
+    
+}
