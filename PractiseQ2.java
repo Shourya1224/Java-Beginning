8d68ce243ff8611ee10a1714d686a8dc72d1cc09
Shourya1224 <shourya21m@gmail.com>
2025-06-25 21:13:33 +0530

    Add files via upload

---
+++ PractiseQ2.java
@@ -0,0 +1,14 @@
+
+public class PractiseQ2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        String name = sc.nextname() ;
+
+       System.out.println(name +"Hello , how was your day ");
+
+        
+
+
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,17 @@
+public class Sum {
+    public static void main(String[] args) {
+        
+        System.out.println("The sum of these three numbers is");
+
+    int num1 = 3;
+    int num2 = 4;
+    int num3 = 7;
+    int Sum = num1 + num2 + num3;
+
+
+    System.out.println(Sum );
+
+
+    }
+    
+}
